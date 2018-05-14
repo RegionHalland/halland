be9e224de49ab4371a5488d9c9ea42995a04db19
Joel Sandén <me@joelsanden.se>
2018-05-14 15:18:24 +0200

    fixed URLs

--- app/Traits/Pagination.php
+++ app/Traits/Pagination.php
@@ -16,6 +16,23 @@ trait Pagination
 		$total_pages = $wp_query->max_num_pages;
 		$current_page = max(1, get_query_var('paged'));
 
+		$previous_link = $current_page - 1;
+		$next_link = $current_page + 1;
+
+		if($previous_link < 0) {
+				$previous_link = NULL;
+		} else {
+			$previous_link = get_pagenum_link($previous_link);
+		}
+
+
+
+		if($next_link > $total_pages) {
+			$next_link = NULL;
+		} else {
+			$next_link = get_pagenum_link($next_link);
+		}
+
 		$pages = paginate_links(array(
 	      'base' => get_pagenum_link(1) . '%_%',
 	      'format' => 'sida/%#%',
@@ -28,10 +45,10 @@ trait Pagination
 
 		$pagination = (object) array(
 			'previous_label' => 'Föregående sida',
-			'previous_link' => 0,
+			'previous_link' => $previous_link,
 			'pages' => $pages,
 			'next_label' => 'Nästa sida',
-			'next_link' => 2,
+			'next_link' => $next_link,
 		);
 
 		return $pagination;
--- resources/views/partials/pagination.blade.php
+++ resources/views/partials/pagination.blade.php
@@ -4,16 +4,18 @@
 		<div class="clearfix mxn2">
 			<div class="col col-12 px2">
 			<ol class="pagination clearfix flex justify-between items-baseline" itemscope itemtype="http://schema.org/SiteNavigationElement">
-				@if(isset($pagination->previous_link))
+
 					<li class="flex-auto left-align">
+						@if(isset($pagination->previous_link))
 						<a class="mr1" href="{{ $pagination->previous_link }}" >
 							<svg class="icon-badge icon-badge--md">
 								<use xlink:href="#arrow-left"/>
 							</svg>
 							{!! $pagination->previous_label !!}
 						</a>
+						@endif
 					</li>
-				@endif
+
 				<li class="flex-auto center">
 					<ol class="list list--inline">
 						@foreach ($pagination->pages as $page)
@@ -22,16 +24,18 @@
 					</ol>
 
 				</li>
-				@if(isset($pagination->next_link))
+
 					<li class="flex-auto right-align">
+						@if(isset($pagination->next_link))
 						<a class="ml1" href="{{ $pagination->next_link }}" >
 							{!! $pagination->next_label !!}
 							<svg class="icon-badge icon-badge--md">
 								<use xlink:href="#arrow-right"/>
 							</svg>
 						</a>
+							@endif
 					</li>
-				@endif
+
 			</ol>
 			</div>
 		</div>
