435d653e082db4b20b7ffc3024f8e54d4c53d477
David Öhlin <hello@davidohl.in>
2018-05-23 08:54:43 +0200

    allow hyphens and dots in usernames

--- app/filters.php
+++ app/filters.php
@@ -34,6 +34,23 @@ add_filter('excerpt_more', function () {
 });
 
 /**
+ * Allow hypenated usernames
+ */
+add_filter( 'wpmu_validate_user_signup', function( $result ) {
+    $error_name = $result[ 'errors' ]->get_error_message( 'user_name' );
+    if ( !empty ( $error_name ) 
+        && $error_name == __( 'Usernames can only contain lowercase letters (a-z) and numbers.' ) 
+        && $result['user_name'] == $result['orig_username'] 
+        && ! preg_match( '/[^-a-z0-9.]/', $result['user_name'] ) 
+    ) {
+        unset ( $result[ 'errors' ]->errors[ 'user_name' ] );
+        return $result;
+    } else {
+        return $result;
+    }
+});
+
+/**
  * Template Hierarchy should search for .blade.php files
  */
 collect([
