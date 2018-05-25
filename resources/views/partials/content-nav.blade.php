b70abf8053028c3aeabba95f1e4e92d5ac01b8b1
Viktor Sarge - Region Halland <viktor.sarge@regionhalland.se>
2018-05-25 12:38:01 +0200

    fixing validation

--- resources/views/partials/content-nav.blade.php
+++ resources/views/partials/content-nav.blade.php
@@ -2,9 +2,9 @@
 <nav class="content-nav-container">
 	<div class="content-nav">
 	<h4>Hitta på sidan</h4>
-	<ul class="content-nav__list">
+	<ul class="content-nav__list" itemscope itemtype="http://schema.org/ItemList">
 		@foreach ($content_nav as $item)
-		<li class="content-nav__item" itemprop="itemListElement" itemtype="http://schema.org/ListItem">
+		<li class="content-nav__item" itemprop="itemListElement">
 			<a class="content-nav__link" href="#{{ $item['slug'] }}">{!! $item['content'] !!}</a>
 			<meta itemprop="position" content="{{ $loop->iteration }}" />
 		</li>
