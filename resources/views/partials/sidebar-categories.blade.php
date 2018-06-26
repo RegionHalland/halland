22467e082363833e460e960593d6d3e5fee20a9b
David Öhlin <hello@davidohl.in>
2018-06-26 16:54:28 +0200

    use traits for categories and tags :open_file_folder:

--- resources/views/partials/sidebar-categories.blade.php
+++ resources/views/partials/sidebar-categories.blade.php
@@ -1,9 +1,8 @@
-@if($categories)
-	<aside class="mb2">
-		<h3>Kategorier</h3>
-			@foreach ($categories as $item) 
-				<a href="{{ $item['href'] }}">{{ $item['label'] }}</a><span class="small light">({{ $item['count'] }})</span>@if (!$loop->last), @endif
-			@endforeach
-		
-	</aside>
+@if($categories && !empty($categories))
+<aside class="mb2">
+	<h3>Kategorier</h3>
+	@foreach ($categories as $category) 
+		<a href="{{ $category['link'] }}">{{ $category['name'] }}</a><span class="small light">({{ $category['count'] }})</span>@if (!$loop->last), @endif
+	@endforeach
+</aside>
 @endif
