c2bc2194ab2dc183cb23fa6da542d16a3c42a035
Viktor Sarge <viktor.sarge@regionhalland.se>
2018-08-01 13:00:43 +0200

    New cookie notice

--- resources/views/partials/cookie-notice.blade.php
+++ resources/views/partials/cookie-notice.blade.php
@@ -1,14 +1,19 @@
 @if (isset($cookie_notice))
-<div id="cookie-notice" class="notice background-dark-blue py2 relative z4">
+<div id="cookie-notice" class="notice py2 relative z4 background-light-blue">
 	<div class="container mx-auto">
 		<div class="clearfix mxn2">
 			<div class="col col-12 px3">
-				<div class="notice__text text-white">
+				<div class="cookie-notice__column cookie-notice__column-left">
+					&#9432;
+				</div>
+				<div class="notice__text cookie-notice__text cookie-notice__column cookie-notice__column-center">
 					{!! $cookie_notice['message'] !!}
 				</div>
-				<button id="cookie-consent" class="strong text-white notice__button">
-					{!! $cookie_notice['button'] !!}
-				</button>
+				<div class="cookie-notice__column cookie-notice__column-right">
+					<button id="cookie-consent" class="strong background-dark-blue text-white notice__button cookie-notice__button">
+						{!! $cookie_notice['button'] !!}
+					</button>
+				</div>
 			</div>
 		</div>
 	</div>
