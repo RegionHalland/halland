47e23aab90e34f3bc5c2ec4ef9fb36e4077c1cb7
David Öhlin <hello@davidohl.in>
2018-08-07 14:20:51 +0200

    minor tweaks

--- resources/views/partials/cookie-notice.blade.php
+++ resources/views/partials/cookie-notice.blade.php
@@ -1,4 +1,4 @@
-@if (isset($cookie_notice))
+{{-- @if (isset($cookie_notice))
 <div id="cookie-notice" class="notice py2 relative z4 background-light-blue">
 	<div class="container mx-auto">
 		<div class="clearfix mxn2">
@@ -13,6 +13,26 @@
 					<button id="cookie-consent" class="strong background-dark-blue text-white notice__button cookie-notice__button">
 						{!! $cookie_notice['button'] !!}
 					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endif --}}
+
+@if (isset($cookie_notice))
+<div id="cookie-notice" class="relative background-light-blue p3 z4">
+	<div class="container mx-auto">
+		<div class="clearfix mxn3">
+			<div class="cookie-notice__container">
+				<div class="cookie-notice__text-container px3">
+					<svg class="icon mr1">
+						<use xlink:href="#info"/>
+					</svg>
+					<span class="h5">{!! $cookie_notice['message'] !!}</span>
+				</div>
+				<div class="cookie-notice__btn-container px3">
+					<button id="cookie-consent" class="btn btn-primary">{!! $cookie_notice['button'] !!}</button>
 				</div>
 			</div>
 		</div>
