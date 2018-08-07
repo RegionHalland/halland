50f8dc38a99afc79bfa8c664aae87a876157384c
David Öhlin <hello@davidohl.in>
2018-08-07 15:00:16 +0200

    remove old cookie-notice

--- resources/views/partials/cookie-notice.blade.php
+++ resources/views/partials/cookie-notice.blade.php
@@ -1,25 +1,3 @@
-{{-- @if (isset($cookie_notice))
-<div id="cookie-notice" class="notice py2 relative z4 background-light-blue">
-	<div class="container mx-auto">
-		<div class="clearfix mxn2">
-			<div class="col col-12 px3">
-				<div class="cookie-notice__column cookie-notice__column-left">
-					&#9432;
-				</div>
-				<div class="notice__text cookie-notice__text cookie-notice__column cookie-notice__column-center">
-					{!! $cookie_notice['message'] !!}
-				</div>
-				<div class="cookie-notice__column cookie-notice__column-right">
-					<button id="cookie-consent" class="strong background-dark-blue text-white notice__button cookie-notice__button">
-						{!! $cookie_notice['button'] !!}
-					</button>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-@endif --}}
-
 @if (isset($cookie_notice))
 <div id="cookie-notice" class="relative background-light-blue p3 z4">
 	<div class="container mx-auto">
