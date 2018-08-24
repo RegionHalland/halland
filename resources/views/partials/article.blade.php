8ba5a3b04ccbb0834eb976b74245ef7e25c26110
Viktor Sarge <viktor.sarge@regionhalland.se>
2018-08-24 08:46:11 +0200

    WIP new look for tables

--- resources/views/partials/article.blade.php
+++ resources/views/partials/article.blade.php
@@ -1,3 +1,60 @@
 <article @php(post_class('article'))>
 @php(the_content())
+
+<div style="position: relative;">
+	<div class="table__sticky-column-container" style="overflow-x: auto">
+		<table>
+			<thead>
+				<tr>
+					<th class="table__sticky-column"><span class="sticky-column__vertical-center">Läkemdelsnamn</span></th>		
+					<th>Substans</th>
+					<th>Indikation</th>
+					<th>Införa eller avstå</th>
+					<th>Regionalt beslut</th>
+					<th>Rek. NT-råd</th>
+				</tr>
+			</thead>
+			<tr>
+				<td class="table__sticky-column"><span class="sticky-column__vertical-center">Intuniv</span></td>
+					<td>guanfacin</td>
+					<td>ADHD</td>
+					<td>Införa</td>
+					<td>2016-07-21</td>
+					<td>2016-05-03</td>
+			</tr>
+			<tr>
+				<td class="table__sticky-column"><span class="sticky-column__vertical-center">Cinqaero</span></td>
+				<td>reslizumab</td>
+				<td>Eosinofil astma</td>
+				<td>Införa</td>
+				<td>2017-11-01</td>
+				<td>2017-06-19</td>		
+			</tr>
+			<tr>
+				<td class="table__sticky-column"><span class="sticky-column__vertical-center">Entresto</span></td>
+				<td>sakubitril/vasartan</td>
+				<td>Hjärtsvikt</td>
+				<td>Införa</td>
+				<td>2016-07-21</td>
+				<td>2016-04-07</td>		
+			</tr>
+			<tr>
+				<td class="table__sticky-column"><span class="sticky-column__vertical-center">Jinarc</span></td>
+				<td>tolvaptan</td>
+				<td>Autosomal dominant ...</td>
+				<td>Införa</td>
+				<td>2018-02-26</td>
+				<td>2017-05-06</td>		
+			</tr>
+			<tr>
+				<td class="table__sticky-column"><span class="sticky-column__vertical-center">Keytruda</span></td>
+				<td>pembrolizumba</td>
+				<td>Icke småcellig</td>
+				<td>Införa</td>
+				<td>2019-02-01</td>
+				<td>2019-04-20</td>		
+			</tr>
+		</table>
+	</div>
+</div>
 </article>
