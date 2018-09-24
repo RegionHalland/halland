e748719e115e5ae9243aca9f9c655d49c133d6de
David Öhlin <hello@davidohl.in>
2018-09-24 08:46:32 +0200

    remove bold from active item

--- resources/views/partials/breadcrumbs.blade.php
+++ resources/views/partials/breadcrumbs.blade.php
@@ -5,7 +5,7 @@
 			<nav class="col col-12 px3">
 			<ol class="breadcrumbs" aria-label="Länkstig" itemscope itemtype="http://schema.org/BreadcrumbList">
 				@foreach ($breadcrumbs as $breadcrumb)
-					<li class="breadcrumbs__item @if ($loop->last) active @endif" @if ($loop->last) aria-current="page" @endif itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
+					<li class="breadcrumbs__item" @if ($loop->last) aria-current="page" @endif itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
 						@if ($breadcrumb['url'])
 							<a class="breadcrumbs__link" href="{{ $breadcrumb['url'] }}">{!! $breadcrumb['name'] !!}</a>
 						@else
