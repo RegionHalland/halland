48c88400cb4a0c943d2eb90e6bfa4c5f52c87eff
Viktor Sarge <viktor.sarge@regionhalland.se>
2018-10-02 13:25:33 +0200

    SVG with correct classes to make them into icons

--- resources/views/partials/new_nav-front.blade.php
+++ resources/views/partials/new_nav-front.blade.php
@@ -2,7 +2,7 @@
 <li class="col-12 lg-col-4">
 	<div class="p3">
 		<span class="center strong">
-			<svg class="">
+			<svg class="icon-badge icon-badge--md mr1">
 		 		<use xlink:href="#chevron-right"/>
 			</svg>
 			<a href="{{ get_page_link($topLevelPage->ID) }}" style="vertical-align: middle; color: #004B93">{{ $topLevelPage->post_title }}</a>
