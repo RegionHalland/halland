9152e13338cdc10b8ca9d5b4204f98385435a412
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-10-11 10:38:44 +0200

    Add possibility to enable custom post types 'nyheter'

--- app/Theme/CustomPostTypes.php
+++ app/Theme/CustomPostTypes.php
@@ -7,60 +7,66 @@ class CustomPostTypes
 
 	public function __construct()
 	{
+		// Define our custom post types
 		$this->custom_post_types = array(
-			array(
-				"post_name"  => "news",
-				"post_label" => "Nyheter"
-			),
-			array(
-				"post_name"  => "puff",
-				"post_label" => "Puffar"
-			),
+			"news"  => array(
+				"name" => "Nyheter",
+				"singular_name" => "Nyhet",
+				'rewrite' => "nyheter",
+				'has_archive' => true
+			)
 		);
 		
-		$this->populateAcfAlternatives();
-		// acf/update_value - filter for every field
-		add_filter('acf/update_value', array($this, 'my_acf_update_value'), 10, 3 );
+		add_action('init', array($this, 'register_custom_post_types'));
+		$this->populate_acf_alternatives();
 	}
 
-	public function populateAcfAlternatives() {
-		/**
-		 * Add users list to the data_curator field
-		 */
+	/**
+	 * Add available custom post types to the activated_custom_post_types field
+	*/
+	public function populate_acf_alternatives()
+	{
 		add_filter('acf/load_field/name=activated_custom_post_types', function($field) {	
 			$field['choices'] = array();
 			
-			foreach ($this->custom_post_types as $custom_post_type) {
-				$field['choices'][$custom_post_type["post_name"]] = $custom_post_type["post_label"];
+			foreach ($this->custom_post_types as $key => $value) {
+				$field['choices'][$key] = $value["name"];
 			}
 		
 			return $field;
 		});
 	}
 
-	function my_acf_update_value( $value, $post_id, $field  ) {
-	    // only do it to certain custom fields
-	    if( $field['name'] == 'activated_custom_post_types' ) {
-	        // get the old (saved) value
-	        $old_value = get_field('field_5bbe0f502774c', $post_id);
-	        
-	        // get the new (posted) value
-	        $new_value = $_POST['acf']['field_5bbe0f502774c'];
-	        
-	        // check if the old value is the same as the new value
-	        if( $old_value != $new_value ) {
-	            // Do something if they are different
-	        	$this->registerCustomPostTypes($old_value, $new_value);
-	        } else {
-	            // Do something if they are the same
-	        }
+	/**
+	 * Register chosen custom post types
+	*/
+	public function register_custom_post_types()
+	{
+		if (function_exists('get_field')) {
+			$post_types_to_activate = get_field('field_5bbe0f502774c', 'option');
+			
+			if(isset($post_types_to_activate) && is_array($post_types_to_activate)) {
+				foreach ($post_types_to_activate as $key => $value) {
+					$post_name = $value;
+					$post_label = $this->custom_post_types[$value]["name"];
+
+					$args = array(
+						'labels' => array(
+							'name'			=> _x($post_label, 'post type general name', 'halland' ),
+							'singular_name'	=> _x($post_label, 'post type singular name', 'halland' ),
+							'menu_name'		=> _x($post_label, 'admin menu', 'halland' ),
+						),
+						'rewrite' => array('slug' => strtolower($post_label)),
+						'public' => true,
+						'taxonomies' => array('category'),
+						
+					);
+
+					register_post_type($post_name, $args);
+				}
+			}
+			
 	    }
-
-		// don't forget to return to be saved in the database
-	    return $value;
 	}
 
-	public function registerCustomPostTypes($new_value) {
-		die(print_r("register" . print_r($new_value)));
-	}
 }
