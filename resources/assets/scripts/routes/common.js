0152191753b8aca8fff69862ab5ac123adb78c19
Viktor Sarge <viktor.sarge@regionhalland.se>
2018-10-15 10:07:12 +0200

    Closing when clicking overlay

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,6 @@
 export default {
 	init() {
-		$( ".burger-menu__button" ).on( "click", function() {
+		$( ".burger-menu__button, .overlay" ).on( "click", function() {
 			$(".burger-dropdown__container").toggle();
 			$(".overlay").toggle();
 			$(".burger-menu__burger-icon").toggle();
