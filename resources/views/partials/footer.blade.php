8367a9c19e6a53fb4c509c8e02593cb6a49520d5
David Öhlin <hello@davidohl.in>
2018-10-17 17:00:12 +0200

    wip: footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,60 @@
-<footer>
-	<div class="container mx-auto">
-		<div class="clearfix">
+<footer class="bg-grey-lightest pb-8 pt-16">
+	<div class="container mx-auto px-4">
+		<div class="w-full md:w-11/12 flex flex-wrap items-stretch mx-auto">
+			<div class="w-full -mx-4">
+				<div class="w-full md:w-6/12 lg:w-4/12 px-4 mb-8">
+					<h5 class="text-xl mb-4">Hej</h5>
+					<ul class="list-reset">
+						<li><a href="#">Så fungerar kommunen</a></li>
+						<li><a href="#">Kommunens mål och budget</a></li>
+						<li><a href="#">Organisation</a></li>
+						<li><a href="#">Ämnen</a></li>
+						<li><a href="#">Publikationer</a></li>
+						<li><a href="#">Nyheter och pressmeddelanden</a></li>
+						<li><a href="#">Engagera dig och påverka</a></li>
+						<li><a href="#">Anslagstavla för beslut och kungörelser</a></li>
+					</ul>
+				</div>
+				<div class="w-full md:w-6/12 lg:w-4/12 px-4 mb-8">
+					<h5 class="text-xl mb-4">Hej</h5>
+					<ul class="list-reset">
+						<li><a href="#">Så fungerar kommunen</a></li>
+						<li><a href="#">Kommunens mål och budget</a></li>
+						<li><a href="#">Organisation</a></li>
+						<li><a href="#">Ämnen</a></li>
+						<li><a href="#">Publikationer</a></li>
+						<li><a href="#">Nyheter och pressmeddelanden</a></li>
+						<li><a href="#">Engagera dig och påverka</a></li>
+						<li><a href="#">Anslagstavla för beslut och kungörelser</a></li>
+					</ul>
+				</div>
+				<div class="w-full md:w-6/12 lg:w-4/12 px-4 mb-8">
+					<h5 class="text-xl mb-4">Hej</h5>
+					<ul class="list-reset">
+						<li><a href="#">Så fungerar kommunen</a></li>
+						<li><a href="#">Kommunens mål och budget</a></li>
+						<li><a href="#">Organisation</a></li>
+						<li><a href="#">Ämnen</a></li>
+						<li><a href="#">Publikationer</a></li>
+						<li><a href="#">Nyheter och pressmeddelanden</a></li>
+						<li><a href="#">Engagera dig och påverka</a></li>
+						<li><a href="#">Anslagstavla för beslut och kungörelser</a></li>
+					</ul>
+				</div>
+				<div class="w-full md:w-6/12 lg:w-4/12 px-4 mb-8">
+					<h5 class="text-xl mb-4">Hej</h5>
+					<ul class="list-reset">
+						<li><a href="#">Så fungerar kommunen</a></li>
+						<li><a href="#">Kommunens mål och budget</a></li>
+						<li><a href="#">Organisation</a></li>
+						<li><a href="#">Ämnen</a></li>
+						<li><a href="#">Publikationer</a></li>
+						<li><a href="#">Nyheter och pressmeddelanden</a></li>
+						<li><a href="#">Engagera dig och påverka</a></li>
+						<li><a href="#">Anslagstavla för beslut och kungörelser</a></li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	</div>
 </footer>
