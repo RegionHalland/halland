07e9abfe5f866ade1753a4bff6f707e790abfc33
David Öhlin <hello@davidohl.in>
2018-10-22 15:03:59 +0200

    add tinymce table and filter content

--- app/Theme/Filters.php
+++ app/Theme/Filters.php
@@ -2,6 +2,8 @@
 
 namespace App\Theme;
 
+use Sunra\PhpSimple\HtmlDomParser;
+
 class Filters
 {
 	public function __construct()
@@ -10,8 +12,12 @@ class Filters
 		add_action('excerpt_more', array($this, 'update_excerpt_more'));
 
 		// TinyMCE Plugins
-		add_filter( 'mce_buttons', array($this, 'addTableButton') );
-		add_filter( 'mce_external_plugins', array($this, 'addTablePlugin') );
+		add_filter( 'mce_buttons', array($this, 'add_table_button') );
+		add_filter( 'mce_external_plugins', array($this, 'add_table_plugin') );
+		
+		// Content
+		add_filter( 'the_content', array($this, 'auto_wrap_tables') );
+		add_filter( 'the_content', array($this, 'add_id_to_headings') );
 	}
 
 	/**
@@ -27,7 +33,7 @@ class Filters
 	 * Add Table button to TinyMCE Editor
 	 * @return array
 	 */
-	public function addTableButton( $buttons )
+	public function add_table_button( $buttons )
 	{
 		array_push( $buttons, 'separator', 'table' );
 		return $buttons;
@@ -37,9 +43,48 @@ class Filters
 	 * Add Table Plugin to TinyMCE Editor
 	 * @return array
 	 */
-	public function addTablePlugin( $plugins )
+	public function add_table_plugin( $plugins )
 	{
 		$plugins['table'] = 'https://unpkg.com/tinymce@latest/plugins/table/plugin.min.js';
 		return $plugins;
 	}
+
+	/**
+    * Wrap tables in div to prevent overflow.
+    * @return object
+    */
+    function auto_wrap_tables( $content )
+    {
+        $content = HtmlDomParser::str_get_html($content);
+        
+        if ($content) {
+            foreach ($content->find('table') as $element) 
+                $element->outertext = '<div class="table-container">' . $element . '</div>';
+        }
+
+        return $content;
+    }
+
+	/**
+    * Parses post content and adds slugified inner text of headings as ID on each heading
+    * @return object
+    */
+    function add_id_to_headings( $content )
+    {
+        if (strlen($content) <= 0) {
+            return;
+        }
+
+        $content = HtmlDomParser::str_get_html($content);
+        
+        foreach ($content->find('h2, h3, h4') as $element) {
+            $slug = sanitize_title($element->innertext);
+            $element->id = $slug;
+        }
+
+        return $content;
+    }
+
+
+
 }
