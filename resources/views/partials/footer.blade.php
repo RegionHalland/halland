8ed0639e6b189b565388eb14918543a81d565127
David Öhlin <hello@davidohl.in>
2018-10-22 09:45:27 +0200

    working footer :football:

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,61 +1,24 @@
+@php($footerContent = get_field('footer_content', 'options'))
+@if(isset($footerContent) && !empty($footerContent))
 <footer class="bg-grey-lightest pb-8 pt-16">
 	<div class="container mx-auto px-4">
-		<div class="w-full md:w-11/12 flex flex-wrap items-stretch mx-auto">
-			<div class="w-full -mx-4">
+		<div class="w-full md:w-11/12 mx-auto">
+			<div class="w-full flex flex-wrap items-stretch -mx-4">
+				@foreach($footerContent as $column)
+				@if(isset($column) && !empty($column))
 				<div class="w-full md:w-6/12 lg:w-4/12 px-4 mb-8">
-					<h5 class="text-xl mb-4">Hej</h5>
+					<h5 class="text-xl mb-4">{{ $column['title'] }}</h5>
 					<ul class="list-reset">
-						<li><a href="#">Så fungerar kommunen</a></li>
-						<li><a href="#">Kommunens mål och budget</a></li>
-						<li><a href="#">Organisation</a></li>
-						<li><a href="#">Ämnen</a></li>
-						<li><a href="#">Publikationer</a></li>
-						<li><a href="#">Nyheter och pressmeddelanden</a></li>
-						<li><a href="#">Engagera dig och påverka</a></li>
-						<li><a href="#">Anslagstavla för beslut och kungörelser</a></li>
+						@foreach($column['list'] as $item)
+						<li><a href="{{ $item['link']['url'] }}">{{ $item['link']['title'] }}</a></li>
+						@endforeach
 					</ul>
 				</div>
-				<div class="w-full md:w-6/12 lg:w-4/12 px-4 mb-8">
-					<h5 class="text-xl mb-4">Hej</h5>
-					<ul class="list-reset">
-						<li><a href="#">Så fungerar kommunen</a></li>
-						<li><a href="#">Kommunens mål och budget</a></li>
-						<li><a href="#">Organisation</a></li>
-						<li><a href="#">Ämnen</a></li>
-						<li><a href="#">Publikationer</a></li>
-						<li><a href="#">Nyheter och pressmeddelanden</a></li>
-						<li><a href="#">Engagera dig och påverka</a></li>
-						<li><a href="#">Anslagstavla för beslut och kungörelser</a></li>
-					</ul>
-				</div>
-				<div class="w-full md:w-6/12 lg:w-4/12 px-4 mb-8">
-					<h5 class="text-xl mb-4">Hej</h5>
-					<ul class="list-reset">
-						<li><a href="#">Så fungerar kommunen</a></li>
-						<li><a href="#">Kommunens mål och budget</a></li>
-						<li><a href="#">Organisation</a></li>
-						<li><a href="#">Ämnen</a></li>
-						<li><a href="#">Publikationer</a></li>
-						<li><a href="#">Nyheter och pressmeddelanden</a></li>
-						<li><a href="#">Engagera dig och påverka</a></li>
-						<li><a href="#">Anslagstavla för beslut och kungörelser</a></li>
-					</ul>
-				</div>
-				<div class="w-full md:w-6/12 lg:w-4/12 px-4 mb-8">
-					<h5 class="text-xl mb-4">Hej</h5>
-					<ul class="list-reset">
-						<li><a href="#">Så fungerar kommunen</a></li>
-						<li><a href="#">Kommunens mål och budget</a></li>
-						<li><a href="#">Organisation</a></li>
-						<li><a href="#">Ämnen</a></li>
-						<li><a href="#">Publikationer</a></li>
-						<li><a href="#">Nyheter och pressmeddelanden</a></li>
-						<li><a href="#">Engagera dig och påverka</a></li>
-						<li><a href="#">Anslagstavla för beslut och kungörelser</a></li>
-					</ul>
-				</div>
+				@endif
+				@endforeach
 			</div>
 		</div>
 	</div>
 </footer>
+@endif
 @php(wp_footer())
