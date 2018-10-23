6391ae55dfafb23a6a07207bf61358f4503327fb
David Öhlin <hello@davidohl.in>
2018-10-23 15:38:17 +0200

    add cookie-notice and go to content

--- app/Controllers/App.php
+++ app/Controllers/App.php
@@ -9,7 +9,7 @@ class App extends Controller
 	use Traits\News;
 	use Traits\TopLevelPages;
 	// use Traits\Bloginfo;
-	// use Traits\CookieNotice;
+	use Traits\CookieNotice;
 	// use Traits\Notices;
 	use Traits\Breadcrumbs;
 	// use Traits\NavSite;
