772d6a227f32e4edbe35e2cf342b22e8bbc03bd4
David Öhlin <hello@davidohl.in>
2018-10-23 15:06:15 +0200

    isset on nav sidebar

--- resources/views/partials/nav-sidebar.blade.php
+++ resources/views/partials/nav-sidebar.blade.php
@@ -1,5 +1,7 @@
+@if(isset($nav_sidebar) && !empty($nav_sidebar))
 <div class="border border-grey-lightest rounded overflow-hidden">
 	<ul class="js-sidebar-nav list-reset">
 		@each('partials.child-list', $nav_sidebar, 'item')
 	</ul>
 </div>
+@endif
