916c5b4336d231604b586bec88b010126559a0f9
David Öhlin <hello@davidohl.in>
2018-10-23 14:39:36 +0200

    add semi working sidebar

--- resources/views/partials/child-list.blade.php
+++ resources/views/partials/child-list.blade.php
@@ -1,19 +1,17 @@
-<li class="border-t border-grey-lightest">
-	<div class="block py-4 pl-3 pr-12 w-full relative block truncate overflow-hidden {{ $item->active ? ' bg-blue-light' : '' }}">
-		<a href="{{ the_permalink($item->ID) }}" class="text-black text-lg no-underline hover:underline whitespace-no-wrap">{{ $item->post_title }}</a>
-		@if (!empty($item->children))
-			<button class="inline-flex h-8 w-8 rounded-full bg-blue-light absolute pin-r pin-t mt-2 items-center justify-center mr-2">
-				<svg class="inline-flex h-4 w-4 align-middle">
-					<use xlink:href="#chevron-down"/>
-				</svg>
-			</button>
-		@endif
-	</div>
+<li class="border-t border-grey-lightest relative">
+	<a href="{{ the_permalink($item->ID) }}" class="block py-4 pl-3 pr-12 w-full block truncate overflow-hidden {{ $item->active ? 'active bg-blue-light' : '' }}">
+		<span class="inline-block text-black text-lg no-underline hover:underline whitespace-no-wrap">{{ $item->post_title }}</span>
+	</a>
 	@if (!empty($item->children))
-	    <ul class="list-reset border-l-4 border-blue-dark">
-		    @foreach($item->children as $item)
-				@include('partials.child-list')
-		    @endforeach
-	    </ul>
+	<button class="js-sidebar-nav__toggle-btn  inline-flex h-8 w-8 rounded-full bg-blue-light absolute pin-r pin-t mt-2 items-center justify-center mr-2">
+		<svg class="inline-flex h-4 w-4 align-middle">
+			<use xlink:href="#chevron-down"/>
+		</svg>
+	</button>
+	<ul class="js-sidebar-nav__list  list-reset border-l-4 border-blue-dark hidden">
+	@foreach($item->children as $item)
+		@include('partials.child-list')
+	@endforeach
+	</ul>
 	@endif
 </li>
--- resources/views/partials/nav-sidebar.blade.php
+++ resources/views/partials/nav-sidebar.blade.php
@@ -1,5 +1,5 @@
 <div class="border border-grey-lightest rounded overflow-hidden">
-	<ul class="list-reset">
+	<ul class="js-sidebar-nav list-reset">
 		@each('partials.child-list', $nav_sidebar, 'item')
 	</ul>
 </div>
