7861e43ce1cb47d46b9efdb1f1b5a2d20270ca2e
David Öhlin <hello@davidohl.in>
2018-10-23 17:44:38 +0200

    remove non working icon from blurb

--- resources/views/partials/blurb.blade.php
+++ resources/views/partials/blurb.blade.php
@@ -1,10 +1,5 @@
 <article class="px-6 py-6 bg-green-light rounded">
-	<div class="h-16 w-16 rounded-full flex items-center justify-center bg-green mb-6">
-		<svg class="h-8 w-8">
-			<use xlink:href="#search"/>
-		</svg>
-    </div>
-    <a class="inline-block text-black font-sans" href="">
+    <a class="mt-2 inline-block text-black font-sans" href="{{ $link['url'] }}">
     	<h2 class="mb-4 font-sans">{{ $blurb->post_title }}</h2>
     </a>
     <p class="mb-6 font-sans text-lg leading-tight">{{ the_field('excerpt', $blurb->ID) }}</p>
