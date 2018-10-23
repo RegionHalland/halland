827785bd719f075266a141c16f35bd193464c0ef
David Öhlin <hello@davidohl.in>
2018-10-23 11:55:39 +0200

    wip: style css

--- resources/views/partials/child-list.blade.php
+++ resources/views/partials/child-list.blade.php
@@ -1,7 +1,12 @@
-<ul class="list-reset">
-	<li><a href="{{ the_permalink($item->ID) }}">{{ $item->post_title }}</a></li>
+<ul class="list-reset border-l-4 border-blue-dark">
+	<li class="border-b p-3 border-grey-lightest">
+		<a class="text-black text-lg no-underline" href="{{ the_permalink($item->ID) }}">{{ $item->post_title }}</a>
+		@if (!empty($item->children))
+			<button>Öppna</button>
+		@endif
+	</li>
 	@if (!empty($item->children))
-	    <ul class="list-reset pl-2">
+	    <ul class="list-reset">
 	    @foreach($item->children as $item)
 			@include('partials.child-list')
 	    @endforeach
--- resources/views/partials/nav-sidebar.blade.php
+++ resources/views/partials/nav-sidebar.blade.php
@@ -1,1 +1,3 @@
+<div class="border border-grey-lightest rounded overflow-hidden">
 @each('partials.child-list', $nav_sidebar, 'item')
+</div>
