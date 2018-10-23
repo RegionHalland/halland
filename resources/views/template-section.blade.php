dde356dfefa0384fa883d7816f18dab963d262b8
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-10-23 15:53:40 +0200

    Update to the vanilla way of getting excerpts

--- resources/views/template-section.blade.php
+++ resources/views/template-section.blade.php
@@ -21,6 +21,12 @@
 			</header>
 			<div class="flex flex-wrap items-stretch -mx-4">
 				@foreach($page_children as $page)
+					@php
+						global $post;
+						// Assign your post details to $post (& not any other variable name!!!!)
+						$post = $page;
+						setup_postdata( $post );
+					@endphp
 					<div class="w-full sm:w-6/12 lg:w-4/12 px-4 mb-8">
 						<a href="{{ $page->url }}" class="text-blue-dark">
 							<h3 class="mb-2 text-xl md:text-2xl">{{ $page->post_title }}</h3>
@@ -29,7 +35,7 @@
 							@if(has_excerpt($page->ID)) 
 								{{ $page->acf_excerpt }}
 							@else
-								{{ \App\trim_excerpt($page->post_content) }}
+								{{ html_entity_decode(wp_trim_words(get_the_excerpt(), 10, '...'))  }}
 							@endif
 						</p>
 					</div>
@@ -37,6 +43,11 @@
 			</div>
 		</div>
 	</div>
+	
+	@if (is_active_sidebar('sidebar-article-bottom'))
+		@include('partials.sidebar-article-bottom')
+	@endif
+	
 </div>
 
 @endsection
