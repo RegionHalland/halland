0c7a8cfb7da530e9294394308e0f09428d33b685
David Öhlin <hello@davidohl.in>
2018-10-30 10:26:44 +0100

    remove accidental hidden

--- resources/views/partials/nav-mobile.blade.php
+++ resources/views/partials/nav-mobile.blade.php
@@ -1,6 +1,6 @@
 @if(isset($nav_site) && !empty($nav_site))
 <div class="">
-	<ul class="js-site-nav  list-reset">
+	<ul class="list-reset">
 		@each('partials.nav-mobile-child-list', $nav_site, 'item')
 	</ul>
 </div>
