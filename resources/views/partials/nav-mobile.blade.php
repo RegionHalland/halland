aae7fee029b060bc4a80da2d83a70d8ba2019e9b
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-10-30 13:13:56 +0100

    Add e-services to mobile nav

--- resources/views/partials/nav-mobile.blade.php
+++ resources/views/partials/nav-mobile.blade.php
@@ -3,5 +3,14 @@
 	<ul class="js-sidebar-nav list-reset">
 		@each('partials.child-list', $nav_site, 'item')
 	</ul>
+	<div class="p-4 bg-grey-lightest border-t border-grey-lighter">
+		<a class="flex items-center text-black no-underline mr-6" target="_blank" href="https://etjanster.intra.regionhalland.se/">
+			<span class="inline-flex h-8 w-8 rounded-full bg-green-light items-center justify-center mr-2">
+				<svg class="inline-flex h-4 w-4 align-middle">
+					<use xlink:href="#user"/>
+				</svg>
+			</span>E-tjänster
+		</a>
+	</div>
 </div>
 @endif
