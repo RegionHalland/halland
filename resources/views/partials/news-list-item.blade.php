b767366766fa615865dd80d7ec5e6e7513f431cd
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-11-05 11:17:43 +0100

    Add new time format separator

--- resources/views/partials/news-list-item.blade.php
+++ resources/views/partials/news-list-item.blade.php
@@ -2,7 +2,7 @@
 	<a href="{{ the_permalink() }}" title="" class="text-blue-dark hover:bg-yellow-light focus:bg-yellow-light mb-2 inline-block">
 		<h2 class="text-xl sm:text-2xl leading-tight">{{ get_the_title() }}</h2>
 	</a>
-	<span class="text-base block text-grey-darkest mb-4">Publicerad: <time itemprop="datePublished" datetime="{{ get_the_date('Y/m/d', get_the_id()) }}">{{ get_the_date('Y/m/d', get_the_id()) }}</time></span>
+	<span class="text-base block text-grey-darkest mb-4">Publicerad: <time itemprop="datePublished" datetime="{{ get_the_date('Y-m-d', get_the_id()) }}">{{ get_the_date('Y-m-d', get_the_id()) }}</time></span>
 	<p class="text-lg text-grey-darkest leading-tight mb-4">{!! get_the_excerpt() !!}</p>
 	@if(get_the_terms(get_the_id(), 'category'))
 		@foreach(get_the_terms(get_the_id(), 'category') as $key => $term)
