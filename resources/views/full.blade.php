4a45d1ad605b38e182597765e11d20d2b3406792
JOHNREGIONHALLAND <john.stenlund@regionhalland.se>
2018-12-24 18:23:15 +0100

    full.blade id main

--- resources/views/full.blade.php
+++ resources/views/full.blade.php
@@ -4,14 +4,13 @@
 
 @extends('layouts.app')
 @section('content')
-
-  @while(have_posts()) @php(the_post())
-	<div class="container mx-auto pt-8 md:pt-16">
+	<div class="container mx-auto pt-8 md:pt-16"  id="main">
+    @while(have_posts()) @php(the_post())
 		<div class="w-full mx-auto">
 		<h1>{{ the_title() }}</h1>
 		{!! the_content() !!}
 		</div>
-	</div>
-  @endwhile
+    @endwhile
+</div>
 
 @endsection
